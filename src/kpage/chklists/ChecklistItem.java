c75b2f7034383fa8a5ccc6e4bf7628e358431a9b
Kai Page <kaibug@gmail.com>
2017-07-10 21:35:52 -0400

    Fixed formatting
    I tend to indent with spaces, and Eclipse auto-inserts tabs. The code looked incredibly stupid. I think I'll try to start using tabs. It's fewer keystrokes, in any case.

--- src/kpage/chklists/ChecklistItem.java
+++ src/kpage/chklists/ChecklistItem.java
@@ -1,63 +1,63 @@
 package kpage.chklists;
 
 public class ChecklistItem {
-    private boolean checked;
-    private String name;
-    private String notes;
-    
-    public ChecklistItem(boolean checked, String name) {
-    	this(checked, name, null);
-    }
-    
-    public ChecklistItem(boolean checked, String name, String notes) {
-    	this.checked = checked;
-    	this.name = name;
-    	this.notes = notes;
-    }
-    
-    public void setChecked(boolean checked) {
-    	this.checked = checked;
-    }
-    
-    public boolean getChecked() {
-    	return checked;
-    }
-    
-    public void setName(String name) {
-    	this.name = name;
-    }
-    
-    public String getName() {
-    	return name;
-    }
-    
-    public void setNotes(String notes) {
-    	if (!notes.isEmpty()) this.notes = notes;
-    	else clearNotes();
-    }
-    
-    public void clearNotes() {
-    	this.notes = null;
-    }
-    
-    public String getNotes() {
-    	return notes;
-    }
-    
-    public boolean hasNotes() {
-    	return this.notes != null;
-    }
-    
-    public String getNotesNoNull() {
-    	if (this.notes != null) return this.notes;
-    	return "";
-    }
-    
-    @Override
-    public String toString() {
-    	String ret = getChecked() ? "[*] " : "[ ] ";
-    	ret += getName();
-    	if (hasNotes()) ret += " | " + getNotes();
-    	return ret;
-    }
+	private boolean checked;
+	private String name;
+	private String notes;
+	
+	public ChecklistItem(boolean checked, String name) {
+		this(checked, name, null);
+	}
+	
+	public ChecklistItem(boolean checked, String name, String notes) {
+		this.checked = checked;
+		this.name = name;
+		this.notes = notes;
+	}
+	
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	
+	public boolean getChecked() {
+		return checked;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setNotes(String notes) {
+		if (!notes.isEmpty()) this.notes = notes;
+		else clearNotes();
+	}
+	
+	public void clearNotes() {
+		this.notes = null;
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	
+	public boolean hasNotes() {
+		return this.notes != null;
+	}
+	
+	public String getNotesNoNull() {
+		if (this.notes != null) return this.notes;
+		return "";
+	}
+	
+	@Override
+	public String toString() {
+		String ret = getChecked() ? "[*] " : "[ ] ";
+		ret += getName();
+		if (hasNotes()) ret += " | " + getNotes();
+		return ret;
+	}
 }
